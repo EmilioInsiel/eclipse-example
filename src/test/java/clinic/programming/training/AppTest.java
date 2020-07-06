eefe30571740c7f97774106c02711b21e8176c50
Emilio <emilio.montagnana@insiel.it>
2020-07-06 17:22:07 +0200

    First commit

---
+++ src/test/java/clinic/programming/training/AppTest.java
@@ -0,0 +1,14 @@
+package clinic.programming.training;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		System.out.println("Here");
+	}
+
+}
